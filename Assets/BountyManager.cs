85eb953dd4ad4e20be329026dbde3fc0a6166dbf
craigslisttrashman <fintanneff@gmail.com>
2025-10-06 00:40:16 -0400

    small error fix.
    small error fix.

--- Assets/BountyManager.cs
+++ Assets/BountyManager.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class BountyManager : MonoBehaviour

4ab3aa38d015d6c19eede843979ee268304308cb
David Marshall <dmarsh12@student.fitchburgstate.edu>
2025-10-08 21:49:59 -0400

    created a basic level

---
+++ Assets/FunSun.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FunSun : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 0.4f; // degrees per second
+    private float currentXRot;
+
+    void Start()
+    {
+        currentXRot = transform.rotation.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentXRot += rotationSpeed * Time.deltaTime;
+        if (currentXRot >= 360f) { currentXRot -= 360f; }
+        transform.rotation = Quaternion.Euler(currentXRot, 15, 0);
+    }
+}

9c3163742ebbaf78d409b1e78ee77bc7715ab3f4
craigslisttrashman <fintanneff@gmail.com>
2025-10-11 22:28:23 -0400

    Implemented the start of a match timer.
    Implemented the start of a match timer, as well as made it such that players receive a random target on game start.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 using Unity.Netcode;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using System.Linq;
 
 public class GameManager : NetworkBehaviour
 {
+
+    private bool gameStarted = false;
+    [SerializeField] private float timeLeftInMatch;
+
     [SerializeField] private List<PlayerController> players;
-    bool allClientsLoaded;
 
     private void Start()
     {
@@ -38,8 +42,56 @@ public class GameManager : NetworkBehaviour
                 players[i].gameObject.SetActive(false);
             }
         }
-
         //Initialize Spawner
         NPCManager.Instance.Initialize();
+        // Start Game
+        StartGameRpc();
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void StartGameRpc()
+    {
+        gameStarted = true;
+        if (!IsHost) return;
+        // FOR DEBUG randomly assign everybody a target
+        int ncount = NetworkManager.Singleton.ConnectedClientsList.Count;
+        for (int i = 0; i < ncount; i++)
+        {
+            int roll = Random.Range(0, ncount - 1);
+            if (roll == i) i += 1;
+            if (roll >= ncount) i = 0;
+            UpdatePlayerTargetRpc(
+                NetworkManager.Singleton.ConnectedClientsList[i].ClientId, 
+                NetworkManager.Singleton.ConnectedClientsList[roll].ClientId
+                );
+        }
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void UpdatePlayerTargetRpc(ulong player_id, ulong target_id)
+    {
+        BountyManager.PlayerEntry found_player =
+            BountyManager.Instance.players.FirstOrDefault(pe => pe.PlayerClient == player_id);
+        BountyManager.PlayerEntry found_target =
+            BountyManager.Instance.players.FirstOrDefault(pe => pe.PlayerClient == target_id);
+        found_player.currentTarget = found_target;
+    }
+
+    private void Update()
+    {
+        if (!gameStarted) return;
+        if (timeLeftInMatch <= 0.0f) return;
+        timeLeftInMatch -= Time.deltaTime;
+        if (timeLeftInMatch <= 0.0f)
+        {
+            if (IsHost) EndGameRpc();
+        }
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void EndGameRpc()
+    {
+        Debug.Log("Game has ended!");
+
     }
 }
